77a6278b21dbaa31260e656ae54da574e6523b16
karsonic <karsonic7283@gmail.com>
2018-05-03 01:51:08 -0400

    finished isValidTx

--- ScroogeCoin/scroogecoin/src/main/java/src/TxHandler.java
+++ ScroogeCoin/scroogecoin/src/main/java/src/TxHandler.java
@@ -28,26 +28,39 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        ArrayList<UTXO> ledgerUTXOs = pool.getAllUTXO();
         UTXOPool unclaimedUTXOPool = new UTXOPool(this.pool);
         ArrayList<Output> txOutputs = tx.getOutputs();
+        double inputValue = 0, outputValue = 0;
 
+        // Loop over outputs
         for (int i = 0; i < tx.numOutputs(); i++) {
             UTXO txUTXO = new UTXO(tx.getHash(), i);
-            // (1)
-            if (!ledgerUTXOs.contains(txUTXO)) 
-                return false;
-            // (3)
+            // (1) and (3)
             if (!unclaimedUTXOPool.contains(txUTXO))
                 return false;
             // (4)
             if (txOutputs.get(i).value < 0)
                 return false;
             
+            outputValue += txOutputs.get(i).value;
             unclaimedUTXOPool.removeUTXO(txUTXO);
         }
 
-        // TODO: Verify signatures
+        // Loop over inputs
+        for (int j = 0; j < tx.getInputs().size(); j++) {
+            Input input = tx.getInputs().get(j);
+            UTXO prevUTXO = new UTXO(input.prevTxHash, input.outputIndex);
+            Output prevOutput = this.pool.getTxOutput(prevUTXO);
+            
+            inputValue += prevOutput.value;
+
+            // (2)
+            Crypto.verifySignature(prevOutput.address, tx.getRawDataToSign(j), input.signature);
+        }
+
+        // (5)
+        if (outputValue > inputValue)
+            return false;
     }
 
     /**
